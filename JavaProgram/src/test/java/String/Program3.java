e2f3147a013383069c3ae8cc218bb59774dcf073
Bipendra <Bipendra@LAPTOP-IT0S8H68>
2021-07-26 22:40:43 +0530

    JavaPrograms

---
+++ JavaProgram/src/test/java/String/Program3.java
@@ -0,0 +1,16 @@
+package String;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		for(int i=0; i<10; i++)
+		{
+			//int a=20;
+			System.out.println(a);
+		}
+
+	}
+
+}
